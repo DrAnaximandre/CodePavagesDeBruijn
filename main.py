a7263566e91b52f23c9a681cdd5e051d85243ee5
Paul Lemaître <pool.lemaitre@gmail.com>
2021-12-29 20:39:48 -0500

    goPolo

--- main.py
+++ main.py
@@ -24,42 +24,35 @@ import matplotlib.pyplot as plt
 from parameters import Parameters
 from tiling import outputNextTiling
 from gamma import MappedGammaParameter
+import numpy as np
+
 
 ######################################
 
-def goLivret() :
-
+def goPolo():
     gamma = MappedGammaParameter(
-        N=5,  # Size
-        shift=[-0.260, -0.155, -0.050, 0.055, 0.160],
-        deltashift=[0 for _ in range(5)],
+        N=8,  # Size
+        shift=np.array([10.5, 2, -15.2, 4, -5, 6, 2, 0.05]),
+        deltashift=[0 for _ in range(8)],
         a=0,
-        functiontomap=lambda x: x
+        functiontomap=lambda x: 20*np.sin(2*x)
     )
     p = Parameters(
         GAMMA=gamma,
-        N=5,
-        R=62,
+        N=gamma.N,
+        R=0,
         SAVE=True,
-        SHOW=False,
-        RECTANGLE=True,
+        SHOW=True,
+        RECTANGLE=False,
+        BACKGROUND = "k",
+        STROKECOLOR= "k",
         TILINGDIR="./toto"
     )
 
-
-    while True :
-
-        #for (d, nbl) in [(4,3), (8,6), (15, 8), (30, 16), (60, 33), (100, 55)] :
-        for (d, nbl) in [(4,3), (8,6), (15, 8), (30, 16), (60, 33)] :
-            p.NBL = nbl
-            p.updateDMAX(d)
-            outputNextTiling(p)
-        
-        p.nextGAMMA()
+    for (d, nbl) in [(10,5), (15,12)]:
+        p.NBL = nbl
+        p.updateDMAX(d)
+        outputNextTiling(p)
 
 
-plt.ioff()
-
-
-#go1()
-goLivret()
+goPolo()
