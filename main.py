deeacbf14dcaad53ea1762b4a5451ca667971b8d
polo <pool.lemaitre@gmail.com>
2022-11-18 23:26:51 -0500

    create readme

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-##########################################################################
-#
-#  A simple implementation of the deBuijn method for non periodic tilings of the plane.
-#
-#  Bruijn, de, N. G. (1981). Algebraic theory of Penrose's non-periodic tilings of the plane.
-#  Indagationes Mathematicae, 43(1), 39-66.
-#
-#  See also
-#  https://github.com/neozhaoliang/pywonderland/blob/master/src/aperiodic-tilings/debruijn.py
-#  for another implementation, much more "professional" and pythonic,
-#  but with a different display style.
-#
-#  Another valuable site : https://www.mathpages.com/home/kmath621/kmath621.htm
-#
 ###########################################################################
 #  
 #  Parameters can be adjusted, see the 'parameters' module.
