3c187133d819a73fa13d517ab7b202f21a42e700
polo <pool.lemaitre@gmail.com>
2022-12-03 16:46:17 -0500

    main now does goDemo in Parallel

--- main.py
+++ main.py
@@ -1,30 +1,14 @@
 ###########################################################################
-#  
-#  Parameters can be adjusted, see the 'parameters' module.
-#
 #  Comments and suggestions are welcome. Mail to :  mike.lembitre@gmail.com
-#
-##########################################################################
-import tqdm
-from joblib import Parallel,delayed
+###########################################################################
+
 from go import *
+from utils import ParallelProcessor
 
-kappa = 1 # number of images you want to run in parallel.
-# This loops over
-L = Parallel(n_jobs=-2)(
-    delayed(goPolo)(5,
-                    shift*10,
-                    i) for
-    i, shift in enumerate(range(kappa))
-)
+P = ParallelProcessor()
+P.add(goDemo)
+P.run([3, 4, 5, 7, 9])
 
-
-# goPolo3D(13,
-#          0.0001,
-#          0.5)
-
-#goSimple()
-#goPolo()
 #goLivret() # does not display but saves pdf
 #goLivretVar() # does not display but saves pdf, and does not quit
 
