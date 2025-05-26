7ecd2c1ee645c4fb384238a0fc02514912d28637
Polo <pool.lemaitre@gmail.com>
2025-05-26 11:37:03 -0400

    feat: unclean code for 5 imbricated ACMs

--- imbricated.py
+++ imbricated.py
@@ -6,76 +6,82 @@ import tiling
 import matplotlib.pyplot as plt
 import acm_kruskal
 
-N=8
+N = 8
+NUM_IMBRICATED = 3  # Number of imbricated tilings
 
 gamma = gm.MappedGammaParameter(
     N=N,
     initialShift=1.2345,
-    functionToMap=lambda s, j:  2*np.sin(s-(1 + j) / 9) + 0.1 * j /9 + np.cos(j)
-)
-
-p1 = Parameters(
-    N=N,
-    GAMMA=gamma,
-    DMAX=5,
-    BACKGROUND=[0,0,0],
-    NBL=5,
-    STROKECOLOR='k',
-    save=False,
-    SCALE_LINEWIDTH=15,
-    c=1.2,
-)
-
-p2 = Parameters(
-    N=N,
-    GAMMA=gamma,
-    DMAX=14,
-    BACKGROUND=[1,1,1],
-    NBL=5,
-    STROKECOLOR=[0.8,0.1,0.8],
-    save=False,
-    SCALE_LINEWIDTH=40,
-    c=1.2,
-    SQUARE=False,
+    functionToMap=lambda s, j: 2 * np.sin(s - (1 + j) / 9) + 0.1 * j / 9 + np.cos(j),
 )
 
 
+DMAXs = [4, 8, 12, 16, 20]  # Maximum degrees for each imbricated tiling
 
-graph1, _ = tiling.compute(p1)
-graph2, _ = tiling.compute(p2)
 
-vs1 = graph1.get_vertices()
-xys1 = [graph1.get_xy(v) for v in vs1]
-xys1 = [(round(x, 3), round(y, 3)) for x, y in xys1]
-xs1, ys1 = zip(*xys1)
+# Parameter definitions for each imbricated tiling
+params = []
 
-vs2 = graph2.get_vertices()
-xys2 = [graph2.get_xy(v) for v in vs2]
-xys2 = [(round(x, 3), round(y, 3)) for x, y in xys2]
-xs2, ys2 = zip(*xys2)
+for dmax in DMAXs:
+    params.append(
+        Parameters(
+            N=N,
+            GAMMA=gamma,
+            DMAX=dmax,
+            BACKGROUND=[0, 0, 0],
+            NBL=10,
+            STROKECOLOR="w",
+            save=False,
+            SCALE_LINEWIDTH=dmax*2,
+            c=1.2,
+        )
+    )
+
+# Compute tilings for each parameter set
+graphs = []
+for p in params:
+    graph, _ = tiling.compute(p)
+    graphs.append(graph)
+
+
+# Extract vertex data for each graph
+vertex_data = []
+for graph in graphs:
+    vs = graph.get_vertices()
+    xys = [graph.get_xy(v) for v in vs]
+    xys = [(round(x, 3), round(y, 3)) for x, y in xys]
+    vertex_data.append(xys)
+
+xs = []
+ys = []
+
+for xys in vertex_data:
+    x_temp, y_temp = zip(*xys)
+    xs.append(x_temp)
+    ys.append(y_temp)
+
 
 sx = -4
 sy = 4
 
-edges1 = acm_kruskal.compute(graph1, [sx, sy])
-edges2 = acm_kruskal.compute(graph2, [sx, sy], invert_distance=True)
+# Compute edges using acm_kruskal
+edges = []
+for i in range(len(graphs)):
+    if i == 2:
+        edges.append(acm_kruskal.compute(graphs[i], [sx, sy], invert_distance=True))
+    else:
+        edges.append(acm_kruskal.compute(graphs[i], [sx, sy]))
 
-def plot_edges(x, 
-               y, 
-               edges, 
-               p, 
-               ax, 
-               center, 
-               bonus= [],
-               offset_color=0.5):
-    
+def plot_edges(
+    x, y, edges, p, ax, center, bonus=[], offset_color=0.5
+):  
     for (i, j) in edges:
         if i in bonus or j in bonus:
             pass
         else:
             xi, xj, yi, yj = x[i], x[j], y[i], y[j]
-        
-        outputs.fancy_mplot([xi, xj], [yi, yj], 1, p, ax, center, offset_color)
+            outputs.fancy_mplot([xi, xj], [yi, yj], 1, p, ax, center, offset_color)
+
 
 def find_index(xys1, xys2):
     idx = []
@@ -86,16 +92,20 @@ def find_index(xys1, xys2):
             pass
     return idx
 
-idxss = find_index(xys1, xys2)
+idxss = [[]]
+for i in range(1,len(graphs)):
+    idxss.append(find_index(vertex_data[i-1], vertex_data[i]))
+    
+
+for i in range(len(graphs)):
+    params[i].PREFIX = 2 
 
 
-# p1.PREFIX = 2
-# p2.PREFIX = "bob"
-# p2.BACKGROUND = "k"
+fig, ax = outputs.prepare_display(params[-1])  # Use parameters from the second tiling
 
-fig, ax = outputs.prepare_display(p2)
+for i in range(len(graphs)):
+    if i%2 == 0:
+        plot_edges(xs[i], ys[i], edges[i], params[i], ax, [sx, sy], idxss[i], offset_color=0.054)
+ 
 
-
-plot_edges(xs1, ys1, edges1, p1, ax, [sx, sy], offset_color=0)
-plot_edges(xs2, ys2, edges2, p2, ax, [sx, sy], idxss, offset_color=0)
 plt.show()
